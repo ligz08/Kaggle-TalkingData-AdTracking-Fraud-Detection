8a0e905acefaa703df04cedb1269ade7090be304
Guozhen Li <ligz@ucdavis.edu>
2018-06-10 04:48:28 -0700

    One-hot encode only recurring ip/os/app/device/channel, with label_binarize() rather than OneHotEncoder.

--- python/HERE.py
+++ python/HERE.py
@@ -3,3 +3,4 @@ import os
 PYTHON_SCRIPTS_DIR = os.path.dirname(os.path.realpath(__file__))
 PROJ_ROOT_DIR = os.path.abspath(os.path.join(PYTHON_SCRIPTS_DIR, os.pardir))
 SCRATCH_DIR = os.path.join(PROJ_ROOT_DIR, 'scratch')
+DOWNLOADS_DIR = os.path.join(PROJ_ROOT_DIR, 'downloads')
